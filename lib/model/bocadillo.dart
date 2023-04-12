a9e65fc644933d274fb069d7ce33ce848595b432
Mario <mpariciob@gmail.com>
2023-04-12 23:18:09 +0200

    Add function to disable and enable sandwiches and fixed errors

--- lib/model/bocadillo.dart
+++ lib/model/bocadillo.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 
 class Bocadillo {
-  Bocadillo({this.uid, required this.name, required this.description, required this.photoUrl, required this.price,  this.ingredients});
+  Bocadillo({this.uid, required this.name, required this.description, required this.photoUrl, required this.price,  this.ingredients, required this.active});
 
   String? uid;
   String name;
   String description;
   String photoUrl;
   double price;
+  bool active;
   List<String>? ingredients;
 
   factory Bocadillo.fromJson(Map<String, dynamic> json) => Bocadillo(
@@ -17,18 +18,17 @@ class Bocadillo {
         photoUrl: json["photoUrl"],
         price: json["price"] != null ? json["price"].toDouble() : null,
         ingredients: List<String>.from(json["ingredients"].map((x) => x)),
-   
-       
-        
-        
+        active: json["active"],        
       );
-
+  
   Map<String, dynamic> toJson() => {
         "name": name,
         "description": description,
         "photoUrl": photoUrl,
         "price": price,
-        "ingredients": List<dynamic>.from(ingredients!.map((x) => x))
+        "ingredients": List<dynamic>.from(ingredients!.map((x) => x)),
+        "active": active,
+
 
         
         
