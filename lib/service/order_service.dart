767dcf1877f405ce43d4c357e64b58bc16b249a1
Mario <mpariciob@gmail.com>
2023-06-09 13:30:29 +0200

    Adaptados pedidos al igual que react :)

--- lib/service/order_service.dart
+++ lib/service/order_service.dart
@@ -2,9 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_proyecto_segunda_evaluacion/model/pedido.dart';
 import 'package:flutter_proyecto_segunda_evaluacion/model/pedido_bocadillo.dart';
 
-
-
-
 final String ORDER_COLLECTION_NAME = "orders";
 
 class PedidoService {
@@ -18,7 +15,6 @@ class PedidoService {
         var orderData = doc.data();
         orderData['idOrder'] = doc.id;
         orders.add(Pedido.fromJson(orderData));
-       
       });
     } catch (e) {
       print(e);
@@ -28,36 +24,59 @@ class PedidoService {
   }
 
   // Add an order
-  static Future<void> addOrder(String idClient, String nombre, List<PedidoBocadillo> bocadillos_order, DateTime dateOrder, bool paid) async {
+  static Future<void> addOrder(
+    String idClient,
+    
+    String nombre,
+    String email,
+    List<PedidoBocadillo> bocadillos_order,
+    DateTime dateOrder,
+    bool paid,
+    String estado,
+    double total,
+  ) async {
     Pedido order = Pedido(
       idOrder: '',
       idClient: idClient,
-      email: nombre,
+      nameClient: nombre,
+      email: email,
       bocadillos_order: bocadillos_order,
       dateOrder: dateOrder,
       paid: paid,
+      estado: estado,
+      total: total,
     );
     FirebaseFirestore.instance.collection(ORDER_COLLECTION_NAME).add(order.toJson());
-    
   }
 
   // Update an order
-  static Future<void> updateOrder(String idOrder, String idClient, String nombre, List<PedidoBocadillo> bocadillos_order, DateTime dateOrder, bool paid) {
+  static Future<void> updateOrder(
+    String idOrder,
+    String idClient,
+    String nombre,
+    String email,
+    List<PedidoBocadillo> bocadillos_order,
+    DateTime dateOrder,
+    bool paid,
+    String estado,
+    double total,
+  ) {
     CollectionReference orders = FirebaseFirestore.instance.collection(ORDER_COLLECTION_NAME);
     return orders
         .doc(idOrder)
         .update({
           'idClient': idClient,
-          'nombre': nombre,
+          'nameClient': nombre,
+          'email': email,
           'bocadillos_order': bocadillos_order.map((bocadillo) => bocadillo.toJson()).toList(),
           'dateOrder': dateOrder.toIso8601String(),
           'paid': paid,
+          'estado': estado,
+          'total': total,
         })
         .then((value) => print("Order updated successfully"))
         .catchError((error) => print("Error updating order: $error"));
-        
   }
-  
 
   // Delete an order
   static void deleteOrder(String idOrder) {
